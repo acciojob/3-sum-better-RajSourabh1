5df9692a7eeff2eaf2b574a4d14328c2c0c4785d
Sourabh Kumar <createownpath111@gmail.com>
2023-03-16 14:05:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
 	let min = Number.MAX_VALUE;
+	let ans = 0;
   for(let i=0;i<arr.length;i++){
 	  for(let j=i+1;j<arr.length;j++){
 		  for(let k=j+1;k<arr.length;k++){
 			  let sum = arr[i]+arr[j]+arr[k];
-			  if(Math.abs(sum-target) <= min)
-				  min = sum;
+			  if(Math.abs(sum-target)<=min){
+                      min = Math.abs(sum-target);
+                      ans = sum;
+                  }
 		  }
 	  }
   }
-		return min;
+		return ans;
 }
 
 module.exports = threeSum;
